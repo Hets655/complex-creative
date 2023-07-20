f93e1ff899aca74db6e426f57bf68aabcde39781
Hets655 <hetalshah655@gmail.com>
2023-07-20 12:23:10 +0100

    Blogs changes

--- src/components/pages/Blog/index.js
+++ src/components/pages/Blog/index.js
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 import Axios from 'axios';
 
 const Blog = () => {
-	let { pID } = useParams();
+	let { PId } = useParams();
 	const [pDeatils, setpDeatils] = useState([]);
-	let url = 'https://chandanbrass.com/wp/wp-json/wp/v2/posts/' + pID  + '?_embed';
+	let url = 'https://chandanbrass.com/wp/wp-json/wp/v2/posts/' + PId  + '?_embed';
 	useEffect(()=>{
 		
 		Axios.get(url).then((res)=>{
